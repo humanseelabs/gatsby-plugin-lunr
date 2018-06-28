24bc92baa76e6dffe173ffa2db771d4715dae980
deemaagog <deemaagog@gmail.com>
2018-06-28 17:25:49 +0300

    Babel

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,6 +1,6 @@
 const lunr = require('lunr')
 const { enhanceLunr } = require('./common.js')
-const fs = require('fs-extra')
+const fs = require('fs')
 
 exports.onPostBuild = ({ getNodes }, pluginOptions) => {
   const { languages = [], filterNodes = () => true, fields = [], resolvers = {} } = pluginOptions
