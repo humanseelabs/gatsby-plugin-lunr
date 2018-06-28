1cc260e6fa8736112ecff2eca40093fcd1e5cc0b
deemaagog <deemaagog@gmail.com>
2018-06-28 11:37:50 +0300

    Initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,52 @@
+const lunr = require('lunr')
+const { enhanceLunr } = require('./common.js')
+const fs = require('fs')
+
+exports.onPostBuild = ({ getNodes }, pluginOptions) => {
+  const { languages = [], filterNodes = () => true, fields = [], resolvers = {} } = pluginOptions
+  enhanceLunr(lunr, languages)
+
+  const store = {}
+  const storeFields = fields.filter((f) => f.store === true)
+
+  const index = lunr(function() {
+    if (languages.length === 1) {
+      this.use(lunr[languages[0]])
+    } else if (languages.length > 1) {
+      this.use(lunr.multiLanguage(...languages))
+    }
+
+    this.ref('id')
+    fields.forEach(({ name, attributes = {} }) => {
+      this.field(name, attributes)
+    })
+
+    getNodes()
+      .filter(filterNodes)
+      .forEach((n) => {
+        const fieldResolvers = resolvers[n.internal.type]
+        if (fieldResolvers) {
+          const doc = {
+            id: n.id,
+            ...Object.keys(fieldResolvers).reduce(
+              (prev, key) => ({
+                ...prev,
+                [key]: fieldResolvers[key](n),
+              }),
+              {},
+            ),
+          }
+          this.add(doc)
+
+          store[n.id] = storeFields.reduce(
+            (acc, f) => ({
+              ...acc,
+              [f.name]: doc[f.name],
+            }),
+            {},
+          )
+        }
+      })
+  })
+  fs.writeFileSync(`public/search_index.json`, JSON.stringify({ index, store }))
+}
